2ab2513ddd38b84be3b7e40aa5b22dcb1acbf20d
Johan Herrra <johan.herrera01@correo.usa.edu.co>
2020-09-24 10:00:46 -0500

    Agrego clase triangulo

--- Figuras.py
+++ Figuras.py
@@ -24,4 +24,42 @@ class Rectangulo(Punto):
         return p
     pass
 
+class Triangulo(Punto):
+
+    def __init__(self,izq,der,sup):
+        self.sup = sup;
+        self.izq = izq;
+        self.der = der;
+
+    def hallarPerimetro(self):
+        l1 = self.izq.hallarDistancia(self.der)
+        l2 = self.izq.hallarDistancia(self.sup)
+        l3 = self.der.hallarDistancia(self.sup)
+        p = l1 + l2 + l3
+        return p
+
+    def hallarArea(self):
+        l1 = self.izq.hallarDistancia(self.der)
+        l2 = self.izq.hallarDistancia(self.sup)
+        l3 = self.der.hallarDistancia(self.sup)
+        s = (l1 + l2 + l3) / 2;
+        a = math.sqrt(s * (s - l1) * (s - l2) * (s - l3))
+        return a;
+
+    def determinarTipo(self):
+        l1 = self.izq.hallarDistancia(self.der)
+        l2 = self.izq.hallarDistancia(self.sup)
+        l3 = self.der.hallarDistancia(self.sup)
+        if l1 == l2 and l1 == l3:
+            return "Es equilatero"
+        else:
+            if l1 == l2 or l1 == l2 or l2 == l3:
+                return "Es isosceles"
+            else:
+                return "Es escaleno"
+
+
+
+
+
 
