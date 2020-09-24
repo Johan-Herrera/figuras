0f0ec9c1316d72bbca41bc120b34bf201386c16f
Johan Herrra <johan.herrera01@correo.usa.edu.co>
2020-09-24 09:59:23 -0500

    Agrego clase rectangulo

--- Figuras.py
+++ Figuras.py
@@ -10,3 +10,18 @@ class Punto:
         return d
     pass
 
+class Rectangulo(Punto):
+    def __init__(self,izqInf,derInf,izqSup,derSup):
+        self.izqInf = izqInf
+        self.derInf = derInf
+        self.izqSup = izqSup
+        self.derSup = derSup
+
+    def hallarPerimetro(self):
+        l1 = self.izqInf.hallarDistancia(self.derInf)
+        l2 = self.izqSup.hallarDistancia(self.derSup)
+        p = 2*l1+2*l2
+        return p
+    pass
+
+
